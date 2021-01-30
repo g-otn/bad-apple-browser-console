6ec11171f51584a359c57f907e441ab86be86d94
g-otn <g-otn@hotmail.com>
2021-01-30 04:21:37 -0300

    use batches to generate text, clear firefox console once in a while

--- generate-braille.js
+++ generate-braille.js
@@ -1,24 +1,52 @@
 const { braillefy } = require('img2braille');
 const fs = require('fs');
 
-const asciiWidth = 60;
+const batchSize = 100;
+
+const asciiWidth = 100;
 const asciiOpts = {
   dither: false,
   invert: false,
 };
 
 (async () => {
-  let frameIndex = 1, text = '';
+  let batchIndex = 0;
   console.info('Generating braille art frames with', asciiWidth, 'ascii width');
 
+  // Braillefy and write to file all frames through batches
   do {
-    const result = await braillefy(`./video/frames/frame${(frameIndex++ + '').padStart(4, '0')}.jpeg`, asciiWidth, asciiOpts)
-      .catch(() => null);
-    
-    if (!result) break;
-    else text += result + '\n';
-  } while (true);
+    let text = '', stop = false;
+    const promises = [];
 
-  fs.writeFileSync('video/braille.txt', text);
-  console.info('Written ' + frameIndex + ' "braille art frames" to text file');
+    // Create all promises for the current batch
+    for (let i = 0; i < batchSize; i++) {
+      const frameIndex = batchIndex * batchSize + i + 1;
+      console.log('batch', batchIndex, 'frame', frameIndex)
+
+      // It seems most of expensive work is done synchronously anyways
+      promises.push(braillefy(`./video/frames/frame${(frameIndex + '').padStart(4, '0')}.jpeg`, asciiWidth, asciiOpts)
+
+      // After the last frame is braillefied, the remaining batch promises will all be rejected and then catched
+        .catch(err => { stop = true })); // File not found, no more frames, shouldn't create next batch
+    }
+
+    // Concatenate all braillefied frames from current batch
+    await Promise.all(promises)
+      .then(values => {
+        for (let i = 0; i < values.length; i++) {
+          // Filter only resolved promises (found frames)
+          if (values[i]) text += values[i] + '\n';
+          else break;
+        }
+      });
+
+    // Append concatenated braillefied frames into file, batch per batch
+    fs.appendFileSync('video/braille.txt', text);
+
+    // Stop when at least one of the batch promises got rejected
+    if (stop) break;
+
+    batchIndex++;
+  } while (true);
+  console.info('Written braillefied frames to text file');
 })();
--- public/play.js
+++ public/play.js
@@ -1,50 +1,66 @@
 const button = document.getElementById('play');
 
+const isFirefox = navigator.userAgent.toLowerCase().indexOf('firefox') > -1;
+
 const fps = 30, ms = 1000 / fps;
 let frames;
 
-const playBraille = async () => {
+const stop = () => {
+  window.stopTimer = true;
+  button.innerText = 'Play'
+}
+
+const play = async () => {
+  button.innerText = 'Stop'
   console.clear();
   // i = 0;
 
   window.stopTimer = false;
   doTimer(frames.length * ms, fps, (steps, count) => {
-    if (frames[count]) console.log(frames[count])
-    else window.stopTimer = true;
+    if (frames[count]) {
+      if (isFirefox && (ms * count % 6000) < ms) { // Firefox console slows down a bit if console isn't cleared once in a while
+        console.clear();
+      }
+      console.log(frames[count])
+    } else {
+      stop();
+    }
   });
 };
 
 button.addEventListener('click', e => {
   if (window.stopTimer === undefined || window.stopTimer === true) {
-    playBraille();
-    button.innerText = 'Stop'
+    play();
   } else {
-    window.stopTimer = true;
-    button.innerText = 'Play'
+    stop();
   }
 });
 
 // Fetch, unzip and load text
-button.innerText = 'Fetching...'
-JSZipUtils.getBinaryContent('braille.zip', (err, data) => {
-  if (err) throw err;
-
-  button.innerText = 'Unzipping...'
-  JSZip.loadAsync(data).then(zip => {
-    button.innerText = 'Loading...'
-    zip.file('braille.txt').async('string')
-      .then(content => {
-        frames = content.split('\n\n')
-        delete frames[frames.length - 1] // Delete last empty 'frame' created by split command
-
-        button.innerText = 'Play'
-        button.disabled = false
-
-        // Show tip to properly display frames
-        const helpLine = new Array(frames[0].split('\n')[0].length + 1).join('⣿') + '⣿⣿'
-        console.clear()
-        console.info('%cTweak console width/zoom so that the following line doesn\'t break:', 'font-size: 22pt')
-        console.info('%c' + helpLine + '\n', 'color: cyan; ')
-      })
+const load = () => {
+  button.innerText = 'Fetching...'
+  JSZipUtils.getBinaryContent('braille.zip', (err, data) => {
+    if (err) throw err;
+  
+    button.innerText = 'Unzipping...'
+    JSZip.loadAsync(data).then(zip => {
+      button.innerText = 'Loading...'
+      zip.file('braille.txt').async('string')
+        .then(content => {
+          frames = content.split('\n\n')
+          delete frames[frames.length - 1] // Delete last empty 'frame' created by split command
+  
+          button.innerText = 'Play'
+          button.disabled = false
+  
+          // Show tip to properly display frames
+          const helpLine = new Array(frames[0].split('\n')[0].length + 1).join('⣿') + '⣿⣿⣿⣿⣿⣿⣿⣿'
+          console.clear()
+          console.info('%cTweak console width/zoom so that the following line doesn\'t break:', 'font-size: 22pt')
+          console.info('%c' + helpLine + '\n', 'color: cyan; ')
+        })
+    });
   });
-});
+}
+
+load();
